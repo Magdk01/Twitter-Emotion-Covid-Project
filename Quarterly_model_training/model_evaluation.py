02930d90a640df4f8847bc0a629d75d1b182f954
Eske Haack <eskehaack@gmail.com>
2023-06-09 10:17:48 +0200

    fixed error in review comment

--- Quarterly_model_training/model_evaluation.py
+++ Quarterly_model_training/model_evaluation.py
@@ -2,11 +2,6 @@ from transformers import AutoModelForMaskedLM, AutoTokenizer
 import torch
 import pandas as pd 
 
-def fix_dataset(df):
-    df = df.replace(r'\r+|\t+','', regex=True)
-    df = df.replace(r'\n',' ', regex=True)
-    return df    
-
 def model_evaluation(dataset, model_path = False, tweeteval = "cardiffnlp/twitter-roberta-base-dec2020"):
     if not model_path:
         model_path = "./Quarterly_model_training/models/Q42019_model"
@@ -15,7 +10,7 @@ def model_evaluation(dataset, model_path = False, tweeteval = "cardiffnlp/twitte
     model = AutoModelForMaskedLM.from_pretrained(model_path)
     tokenizer = AutoTokenizer.from_pretrained(tweeteval)
 
-    validation_set = list(fix_dataset(pd.read_csv(dataset, header = 0, delimiter=","))['text'])
+    validation_set = list(pd.read_csv(dataset, header = 0, delimiter=",")['text'])
 
     encoded_inputs = tokenizer(validation_set, truncation=True, padding=True, return_tensors="pt")
     labels = encoded_inputs.input_ids.clone()
