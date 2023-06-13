6174df9383fcb66c9faf0ea2fdef0c366043e6c0
Mikkelthe <76942610+Mikkelthe@users.noreply.github.com>
2023-06-13 11:08:28 +0200

    Code used to evaluate sentiment from finetuned models
    ^
     I
     I
     I

---
+++ Finetuning_to_sentiment_analysis/Evaluation_sentiment_timelm/sentiment_eval_time_lm.py
@@ -0,0 +1,75 @@
+#from transformers import pipeline
+import tweetnlp
+import pandas as pd
+import tqdm
+import os
+import torch
+import sys
+
+"""
+For the "MODEL" parameter use one of:
+    cardiffnlp/twitter-roberta-base-sentiment-latest
+    cardiffnlp/twitter-roberta-base-sentiment
+    
+    cardiffnlp/twitter-roberta-base-2019-90m
+    cardiffnlp/twitter-roberta-base-mar2020
+    cardiffnlp/twitter-roberta-base-jun2020
+    cardiffnlp/twitter-roberta-base-sep2020	
+    cardiffnlp/twitter-roberta-base-dec2020	
+    cardiffnlp/twitter-roberta-base-mar2021	
+    cardiffnlp/twitter-roberta-base-jun2021
+    cardiffnlp/twitter-roberta-base-sep2021
+    cardiffnlp/twitter-roberta-base-dec2021
+    cardiffnlp/twitter-roberta-base-2021-124m
+    cardiffnlp/twitter-roberta-base-mar2022
+    cardiffnlp/twitter-roberta-base-jun2022
+    cardiffnlp/twitter-roberta-base-mar2022-15M-incr
+    cardiffnlp/twitter-roberta-base-jun2022-15M-incr
+    cardiffnlp/twitter-roberta-base-sep2022
+    cardiffnlp/twitter-roberta-base-sep2022
+    cardiffnlp/twitter-roberta-base-2022-154m	
+    cardiffnlp/twitter-roberta-large-2022-154m	
+    
+"""
+
+#Sets the cache folder for the transformer models on the HPC's HOME drive
+#os.environ['TRANSFORMERS_CACHE'] = './cache/'
+
+DATASET = f"{sys.argv[1]}.csv"
+MODEL = sys.argv[2] # See above for example
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print(f'Using Cuda device: {device}\n')
+else:    
+    raise RuntimeError('No CUDA available')
+
+tqdm.tqdm.pandas()
+
+model = tweetnlp.load_model(MODEL)
+#sentiment_task = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL,device=0,truncation=True,max_length=512)
+#print('pipeline loaded')
+
+print('reading dataset...')
+df = pd.read_csv(f'{DATASET}',index_col=0)
+print('df read')
+
+def sentiment_for_df(text):
+	classification = model.predict(text)
+
+	if classification =='negative':
+		return -1
+	elif classification =='neutral':
+		return 0
+	elif classification =='positive':
+		return 1
+	else:
+		return classification
+		
+df['sentiment'] = df['text'].progress_apply(sentiment_for_df)
+
+filename = f'./sentiment_subsample_{MODEL.split("/")[-1]}_{DATASET.split("/")[-1]}'
+print(filename)
+df.to_csv(f'{filename}')
+
+print(f'{filename} saved')
---
+++ Finetuning_to_sentiment_analysis/Evaluation_sentiment_timelm/submit_scripts/submit_2022_Q3.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#BSUB -J test
+#BSUB -o test%J.out
+#BSUB -e test%J.err
+#BSUB -q gpuv100
+#BSUB -gpu "num=1"
+#BSUB -n 1
+#BSUB -R "rusage[mem=6GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -W 02:00
+#BSUB -N
+#BSUB -B
+# end of BSUB options
+
+# load a scipy module
+# replace VERSION and uncomment
+module load scipy/1.6.3-python-3.9.6
+module load cuda/11.7
+
+# activate the virtual environment
+# NOTE: needs to have been built with the same SciPy version above!
+source FRANK/bin/activate
+
+python "sentiment_eval_time_lm.py" "data/quarterly_data_2022_Q3" "sentiment/best_model_10"
