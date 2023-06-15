0fea3a50807860e79987990e2001d76a358d9826
Mikkelthe <s204107@student.dtu.dk>
2023-06-15 18:55:05 +0200

    Script for wordeval
    Makes a csv file with all words evaluated with the probability of being assigned to each class. the probability is denoted in a string contaning class aswell. Therefore this script is meant to create a csv file that can be cleaned by word_eval_cleaner.py

--- Timelm_word_eval.py
+++ Timelm_word_eval.py
@@ -19,7 +19,8 @@ tqdm.tqdm.pandas()
 DATASET = f"{sys.argv[1]}.csv"
 
 model = ['base_model_0','base_model_1','base_model_2','base_model_3','base_model_4','base_model_5',
-		'base_model_6','base_model_7','base_model_8','base_model_9','base_model_10'
+		'base_model_6','base_model_7','base_model_8','base_model_9','base_model_10', 'cardiffnlp/twitter-roberta-base-sentiment-latest',
+		'cardiffnlp/twitter-roberta-base-sentiment'
 		]
 df = pd.read_csv(f'{DATASET}',index_col=0)
 
@@ -28,36 +29,28 @@ def wordstimelm(word):
 		return prediction
 
 for i,MODEL in enumerate(model):
-	model = tweetnlp.load_model('sentiment',MODEL)		
-	df['sentimenttime_lm'+str(i)] = df['keyword'].progress_apply(wordstimelm)
-
-
-def sentiment_scoring(text):
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-    scores = output[0][0].detach().numpy()
-    return scores
-
-premodel = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-postmodel = f"cardiffnlp/twitter-roberta-base-sentiment"   
-
-tokenizer = AutoTokenizer.from_pretrained(premodel)
-config = AutoConfig.from_pretrained(premodel)
-model = AutoModelForSequenceClassification.from_pretrained(premodel)
-
-df['sentimentpre'] = df['keyword'].progress_apply(sentiment_scoring)
-
-tokenizer = AutoTokenizer.from_pretrained(postmodel)
-config = AutoConfig.from_pretrained(postmodel)
-model = AutoModelForSequenceClassification.from_pretrained(postmodel)
-
-df['sentimentpost'] = df['keyword'].progress_apply(sentiment_scoring)
+	if i < 11:
+		model = tweetnlp.load_model('sentiment',MODEL)
+		df['time_lm'+str(i)] = df['keyword'].progress_apply(wordstimelm)
+	elif i == 11:
+		model = tweetnlp.load_model('sentiment',MODEL)
+		df['pre'] = df['keyword'].progress_apply(wordstimelm)
+	else:
+		model = tweetnlp.load_model('sentiment',MODEL)
+		df['post'] = df['keyword'].progress_apply(wordstimelm)	
+		
 
 filename = f'./sentiment_keywords'
 print(filename)
 df.to_csv(f'{filename}')
 
 print(f'{filename} saved')
+
+
+
+
+
+
 
 
 
